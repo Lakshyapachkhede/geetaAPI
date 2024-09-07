e01291a10e2054cc5fd7a44577484d734e01d933
Lakshyapachkhede <pachkhedelakshya@gmail.com>
2024-09-07 08:36:12 +0530

    added remote database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const utils = require('./utils.js')
+require('dotenv').config();
 
 const {connectMongoDb} = require('./connection.js')
 const shlokRouter = require("./routes/shlok.router.js")
@@ -8,8 +9,8 @@ const adhyayRouter = require("./routes/adhyay.router.js")
 const {logReqRes} = require("./middlewares/index.js");
 
 const app = express();
-const IP_ADDRESS = utils.getIPAddress();
-const PORT = 8000;
+// const IP_ADDRESS = utils.getIPAddress();
+const PORT = process.env.PORT;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(logReqRes("log.txt"))
@@ -17,9 +18,9 @@ app.use(logReqRes("log.txt"))
 app.use("/shlok", shlokRouter);
 app.use("/adhyay", adhyayRouter);
 
-connectMongoDb("mongodb://127.0.0.1:27017/geetaAPI").then(()=>console.log("mongodb connected"));
+connectMongoDb(process.env.DB_CONNECTION_STRING).then(()=>console.log("mongodb connected"));
 
 
-app.listen(PORT, IP_ADDRESS, () => {
-    console.log(`Listning at https://${IP_ADDRESS}:${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Listning at ${PORT}`)
 })
